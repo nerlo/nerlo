c1f186b1e234c0725abded9423993ae30eda9bea
Ingo <ingo@karla.lan.local>
2010-01-30 12:08:52 +0100

    update some docs

--- java/src/org/ister/nerlo/Main.java
+++ java/src/org/ister/nerlo/Main.java
@@ -1,11 +1,9 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package org.ister.nerlo;
 
 /**
+ * Main class.
+ * 
+ * We should read node spec from command line and/or conf file.
  *
  * @author ingo
  */
